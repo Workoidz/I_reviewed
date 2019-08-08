607b218a748acac13330b3c044daf664b258e680
Ritesh Kapoor <ritesh@workoidz.com>
2019-08-08 13:32:58 +0530

    Seeding of Reviewers & Books

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Reviewer.create! [
+  { name: "Joe", password_digest: "abc123" },
+  { name: "Jim", password_digest: "123abc" }
+]
+
+
+Book.create! [
+  { name: "Eloquent Ruby", author: "Russ Olsen", reviewer_id: 1 },
+  { name: "Beginning Ruby", author: "Peter Cooper", reviewer_id: 1 },
+  { name: "Metaprogramming Ruby 2", author: "Paolo Perrotta", reviewer_id: 2 },
+  { name: "Design Patterns in Ruby", author: "Russ Olsen", reviewer_id: 1 },
+  { name: "The Ruby Programming Language", author: "David Flanagan", reviewer_id: 2 }
+]
